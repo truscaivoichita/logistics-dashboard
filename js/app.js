371443a68bdaf6150d5594a63c8c298e392ed316
truscaivoichita <sabo_voichita4sacrifice@yahoo.com>
2026-03-10 19:43:09 +0100

    fixed meta and input

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 console.log("Logistics Dashboard started");
+const form = document.getElementById("locationForm");
+const input = document.getElementById("locationInput");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const location = input.value;
+
+  console.log("New delivery location:", location);
+
+  input.value = "";
+});
