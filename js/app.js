5c3ef2098b1d66cf4cdff78d813c7553456586cf
truscaivoichita <sabo_voichita4sacrifice@yahoo.com>
2026-03-10 20:15:41 +0100

    display added delivery locations in list

--- js/app.js
+++ js/app.js
@@ -1,20 +1,25 @@
-console.log("Logistics Dashboard started");
-const form = document.getElementById("locationForm");
-const input = document.getElementById("locationInput");
-
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  const location = input.value;
-
-  console.log("New delivery location:", location);
-
-  input.value = "";
-});
-
 const map = L.map("mapContainer").setView([40.4168, -3.7038], 13);
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution: "&copy; OpenStreetMap",
 }).addTo(map);
+
+console.log("Logistics Dashboard started");
+const form = document.getElementById("locationForm");
+const input = document.getElementById("locationInput");
+const list = document.getElementById("locationsList");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const location = input.value;
+
+  if (!location) return;
+  const li = document.createElement("li");
+  li.textContent = location;
+  list.appendChild(li);
+  console.log("New delivery location:", location);
+
+  input.value = "";
+});
